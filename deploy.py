ce292eeae8f648c53f03e71de322ded1e87a0e6c
Geoff Wyatt <geoffwyatt@gmail.com>
2012-04-20 22:11:15 -0400

    Updated deploy script, added production option

--- deploy.py
+++ deploy.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 
 import os
+from sys import argv
 
-# Clean deployment folder, generate new site, serve via hyde webserver
-os.system("rm -rf deploy/")
-os.system("hyde gen")
-os.system("hyde serve")
+if len(argv) == 2:
+    arg = argv[1]
+else:
+    arg = None
+
+if arg == "prod":
+    os.system("rm -rf deploy/")
+    os.system("hyde gen -c prod.yaml")
+    os.system("hyde serve")
+else:
+    os.system("rm -rf deploy/")
+    os.system("hyde gen")
+    os.system("hyde serve")
